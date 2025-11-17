f8eb11529517e92e83693cd92822fa969f28be54
Jayce Hottenroth <hottenro@usc.edu>
2025-11-17 13:35:32 -0800

    fixing the refresh error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [
-    react()
-    , tailwindcss()],
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // This adds 404.html correctly
+        404: resolve(__dirname, 'index.html'),
+      }
+    }
+  },
+  base: "",
+});
